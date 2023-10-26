f11522d37c9dc67d7fe2438e094ae29d2c5a83ff
junior vargas <juniorvargascovarrub@my.whccd.edu>
2023-10-25 19:54:03 -0700

    I made a code for java script

---
+++ script.js
@@ -0,0 +1,9 @@
+console.log("Hello world");
+console.log("Hello",""," world");
+console.log("Hello \nWorld");
+console.log(3*8+2);
+console.log(10 > 5);
+console.log("apples" != "oranges");
+console.log(10 *10 > 50 && 5 * 5 < 50 );
+console.log(1 + 2 < 5 || 1 * 10 < 5 );
+console.log(8 * "four");
